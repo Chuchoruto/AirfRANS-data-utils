608538bfe13fef9d29c8e4d0aff5b1c413513f2f
Luca <42877969+Chuchoruto@users.noreply.github.com>
2024-10-20 18:14:12 -0700

    Changed import

--- modeling/AirFNN.py
+++ modeling/AirFNN.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from data_manip.manip_utils import ChunkedDataset
+from ..data_manip.manip_utils import ChunkedDataset
 
 class AirFNN(nn.Module):
     """
